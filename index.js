97f2de2be818b77e2961a5fa4e4ab87bec20ebe2
Ryan Buckley <ryanbuckley@gmail.com>
2020-12-12 22:11:34 -0500

    test for single image result

---
+++ index.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const port = 8080;
+const express = require('express');
+const app = express();
+const axios = require('axios');
+
+const asBrowser = { headers: { 'User-Agent': 'Mozilla/5.0' } }
+
+app.get('/getImageURL', async (req, res) => {
+  try {
+    const r = await axios.get(req.query.link, asBrowser)
+    const realURL = r.request.res.responseUrl
+    const realURLres = await axios.get(realURL, asBrowser)
+    const image = realURLres.data
+    console.log(image)
+    res.send(image)
+  } catch (err) { console.log("error: ", err)}
+})
+
+app.get('/search', (req, res) => {
+  const base = 'https://findit.library.yale.edu/?utf8=%25E2%259C%2593&id=digcoll%253A3455720&utf8=%25E2%259C%2593&search_field=all_fields&q='
+  axios
+    .get(base + req.query.q)
+    .then(response => res.send(response.data))
+    .catch(error => res.send(error))
+})
+
+app.get('/', (req, res) => {
+  return res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.listen(port);
+console.log(`listening on ${port}`);
