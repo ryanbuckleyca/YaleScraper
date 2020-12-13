c0b4a974c7ee8426fd542eb861b4e282b13205f0
Ryan Buckley <ryanbuckley@gmail.com>
2020-12-13 01:22:28 -0500

    server needs a rest

--- index.js
+++ index.js
@@ -8,26 +8,35 @@ const asBrowser = { headers: { 'User-Agent': 'Mozilla/5.0' } }
 
 app.get('/getImageURL', async (req, res) => {
   try {
-    const r = await axios.get(req.query.link, asBrowser)
-    const realURL = r.request.res.responseUrl
+    const imagePage = await axios.get(req.query.link, asBrowser)
+    const realURL = imagePage.request.res.responseUrl
     const realURLres = await axios.get(realURL, asBrowser)
-    const image = realURLres.data
-    console.log(image)
-    res.send(image)
-  } catch (err) { console.log("error: ", err)}
+    const hiResFile = realURLres.data
+    res.send(hiResFile)
+  } 
+  catch(err) { 
+    res.send(`error: ${err}`)
+    console.log('error in /search route: ', err) 
+  }
 })
 
-app.get('/search', (req, res) => {
-  const base = 'https://findit.library.yale.edu/?utf8=%25E2%259C%2593&id=digcoll%253A3455720&utf8=%25E2%259C%2593&search_field=all_fields&q='
-  axios
-    .get(base + req.query.q)
-    .then(response => res.send(response.data))
-    .catch(error => res.send(error))
+app.get('/search', async (req, res) => {
+  try {
+    const query = req.query.find
+    const base = 'https://findit.library.yale.edu/?q='
+    const result = await axios.get(base + query, asBrowser)
+    const html = result.data
+    res.send(html)
+  } 
+  catch(err) { 
+    res.send(`error: ${err}`)
+    console.log('error in /search route: ', err) 
+  }
 })
 
-app.get('/?*', (req, res) => {
-  console.log('/?* called')
-  return res.sendFile(path.join(__dirname + `/index.html?${req.query}`))
+app.get('/', (req, res) => {
+  // TODO: if there are no params, render a form?
+  return res.sendFile(path.join(__dirname + '/index.html'))
 })
 
 app.listen(port);
